23c5518f4e0e33f7c55f24f34435c151b60be7c4
Nengak Dakup <nengakdakup@gmail.com>
2024-11-19 16:24:52 +0100

    Add raleway font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,25 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import './globals.css'
+import type { Metadata } from 'next'
+import { Raleway } from 'next/font/google'
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const raleway = Raleway({ 
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+})
 
 export const metadata: Metadata = {
-  title: "Theralink",
-  description: "Theralink",
-};
+  title: 'Theralink',
+  description: 'Theralink - Your Therapy Management Solution',
+}
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={raleway.className}>{children}</body>
     </html>
-  );
+  )
 }
