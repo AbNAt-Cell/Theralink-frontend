3c049fdc99ac441454cfa8ab3bae8a08366fd73f
Nengak Dakup <nengakdakup@gmail.com>
2024-11-25 09:27:48 +0100

    Add ph=pages support

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/Theralink-frontend",
+  assetPrefix: "/Theralink-frontend/",
 };
 
 export default nextConfig;
