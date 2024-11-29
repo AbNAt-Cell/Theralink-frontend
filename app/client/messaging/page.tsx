50d54d839b8e9bf8469515103a9f96b3d33162da
Nengak Dakup <nengakdakup@gmail.com>
2024-11-29 09:45:30 +0100

    Add messaging page

---
+++ app/client/messaging/page.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+
+const ClientMessaging = () => {
+  return (
+    <div className="container max-w-[1350px] mx-auto p-6 space-y-6">
+      <Card>
+        <CardHeader>
+          <CardTitle>
+            Messaging
+          </CardTitle>
+        </CardHeader>
+        <CardContent className='flex'>
+
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
+
+export default ClientMessaging
