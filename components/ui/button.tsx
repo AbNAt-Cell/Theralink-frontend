e1894a540763d35e439b2d201abe4c053b9f8624
Nengak Dakup <nengakdakup@gmail.com>
2025-02-09 17:16:41 +0100

    feat: add outlineSecondary variant to button

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -15,6 +15,8 @@ const buttonVariants = cva(
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
           "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
+        outlineSecondary:
+          "border border-secondary text-secondary shadow-sm hover:bg-secondary hover:text-secondary-foreground",
         pill:
           "text-primary bg-primary/10 shadow-sm hover:bg-primary hover:text-white",
         secondary:
