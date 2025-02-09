d4723715ffac401cdf5610fee18d00f4f9393c11
Nengak Dakup <nengakdakup@gmail.com>
2025-02-09 16:12:12 +0100

    feat: remove api timeout

--- lib/axios.ts
+++ lib/axios.ts
@@ -3,7 +3,6 @@ import axios from 'axios';
 // Create an Axios instance
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-  timeout: 20000,
 });
 
 // Request interceptor to add tokens
