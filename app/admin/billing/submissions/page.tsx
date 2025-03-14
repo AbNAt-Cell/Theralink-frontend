efc42e3ba18eff76217f0eca03b983fbe924840e
Nengak Dakup <nengakdakup@gmail.com>
2025-03-14 17:21:32 +0100

    feat: add billing submission table actions

--- app/admin/billing/submissions/page.tsx
+++ app/admin/billing/submissions/page.tsx
@@ -4,6 +4,14 @@ import { columns } from "./table-columns"
 import type { BillingSubmission } from '@/types/billing';
 import type { Filter } from '@/components/TableFilters';
 import { DataTable } from '@/components/ui/data-table';
+import { Button } from '@/components/ui/button';
+import { ChevronDown, FileOutput } from 'lucide-react';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 const AdminBillingSubmissionsPage = () => {
   const billingSubmissions: Partial<BillingSubmission>[] = [
     {
@@ -41,7 +49,28 @@ const AdminBillingSubmissionsPage = () => {
   return (
     <div className="flex flex-col gap-4">
       <div className="flex flex-col gap-4">
-        <h1 className="text-2xl font-bold">Billing Submission</h1>
+        <div className='flex flex-col md:flex-row justify-between gap-4'>
+          <h1 className="text-2xl font-bold">Billing Submission</h1>
+          <div className='flex flex-row flex-wrap gap-4'>
+            <Button variant="secondary">
+              <FileOutput /> Export to Excel
+            </Button>
+
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <Button variant="outline">
+                  Batch Action <ChevronDown />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem>Mark Selected Batch as Resolved</DropdownMenuItem>
+                <DropdownMenuItem>Mark Selected Batch as Unresolved</DropdownMenuItem>
+                <DropdownMenuItem>Update Billing Status</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+
+          </div>
+        </div>
         <div className='flex flex-row gap-4 justify-between'>
           <div className='py-2 px-3 w-1/4 border-2 border-primary rounded-lg'>
             <p className='text-lg text-primary font-bold'>Batches</p>
